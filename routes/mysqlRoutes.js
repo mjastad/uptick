a569b01205957e5a321c4747c53014d4df3d2e63
Michael Jastad <cattfish00@gmail.com>
2018-03-25 05:08:42 -0700

    Update mysqlRoutes.js

--- routes/mysqlRoutes.js
+++ routes/mysqlRoutes.js
@@ -1,16 +1,25 @@
+var express = require('express');
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
+var mysqlCon = mysql.createConnection({
   host: "10.21.66.101",
   user: "root",
   password: "nutanix/4u",
   database: "Uptick"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT * FROM customers", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-});
+var routes = function(){
+    var mysqlRouter = express.Router();
+
+    mysqlRouter.route('/')
+        .get(function(req, res){
+            mysqlCon.connect(function (err) {
+                 mysqlCon.query('select * from Parts', function (err, result, fields) {
+                      res.json(result);
+	                });
+	          });
+     });
+     return mysqlRouter; 
+};
+
+module.exports = routes;
